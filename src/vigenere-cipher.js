a50253151cd4a97a2012d2ad2495d3a37c1f870c
Yury Bialou <Pradd.by@gmail.com>
2021-02-23 15:21:47 +0300

    feat: add implementation function vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  arr1 = [];
+  arr2 = [];
+  flug = true;
+  constructor(flug = true){
+    this.flug = flug;
+    this.init();
+  }
+  
+  encrypt(str, key) {
+    let ind = 0;
+    let result = str.toUpperCase().split('').map( (it, index) => {
+      if (this.arr1.includes(it))
+        return this.arr2[this.arr1.indexOf(it) + this.arr1.indexOf(key.toUpperCase()[(index-ind) % key.length])];
+      else {
+        ++ind;
+        return it;
+      }
+    })
+    if (this.flug) return result.join('');
+    else return result.reverse().join('');
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(str, key) {
+    let ind = 0;
+    let result = str.toUpperCase().split('').map( (it, index) => {
+      if (this.arr1.includes(it))
+        return this.arr2[this.arr2.lastIndexOf(it) - this.arr1.indexOf(key.toUpperCase()[(index-ind) % key.length])];
+      else {
+        ++ind;
+        return it;
+      }
+    })
+    if (this.flug) return result.join('');
+    else return result.reverse().join('');
+  }
+
+  init () {
+    for (let i = 65; i <= 90; i++) {
+      this.arr1.push(String.fromCodePoint(i));
+    }
+    this.arr2 = [].concat(this.arr1, this.arr1);
   }
 }
 
